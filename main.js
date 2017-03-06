9adc34df74935969915f615b6b8410280729dcc0
Yau Kuen Tsang <danielyktsang@gmail.com>
2017-03-06 03:37:17 -0800

    canvas can draw a grid line.

---
+++ main.js
@@ -0,0 +1,112 @@
+var AM = new AssetManager();
+var WIDTH = 20;
+var HEIGHT = 20;
+var RADIUS = 10;
+
+function Animation(spriteSheet, frameWidth, frameHeight, sheetWidth, frameDuration, frames, loop, scale) {
+    this.spriteSheet = spriteSheet;
+    this.frameWidth = frameWidth;
+    this.frameDuration = frameDuration;
+    this.frameHeight = frameHeight;
+    this.sheetWidth = sheetWidth;
+    this.frames = frames;
+    this.totalTime = frameDuration * frames;
+    this.elapsedTime = 0;
+    this.loop = loop;
+    this.scale = scale;
+}
+
+Animation.prototype.drawFrame = function (tick, ctx, x, y) {
+    this.elapsedTime += tick;
+    if (this.isDone()) {
+        if (this.loop) this.elapsedTime = 0;
+    }
+    var frame = this.currentFrame();
+    var xindex = 0;
+    var yindex = 0;
+    xindex = frame % this.sheetWidth;
+    yindex = Math.floor(frame / this.sheetWidth);
+
+    ctx.drawImage(this.spriteSheet,
+        xindex * this.frameWidth, yindex * this.frameHeight,  // source from sheet
+        this.frameWidth, this.frameHeight,
+        x, y,
+        this.frameWidth * this.scale,
+        this.frameHeight * this.scale);
+}
+
+Animation.prototype.currentFrame = function () {
+    return Math.floor(this.elapsedTime / this.frameDuration);
+}
+
+Animation.prototype.isDone = function () {
+    return (this.elapsedTime >= this.totalTime);
+}
+
+// no inheritance
+// function Background(game, spritesheet) {
+//     this.x = 0;
+//     this.y = 0;
+//     this.spritesheet = spritesheet;
+//     this.game = game;
+//     this.ctx = game.ctx;
+// };
+//
+// Background.prototype.draw = function () {
+//     this.ctx.drawImage(this.spritesheet,
+//         this.x, this.y);
+// };
+//
+// Background.prototype.update = function () {
+// };
+
+
+function Cell(game, x, y) {
+    this.animation = new Animation(spritesheet, 189, 230, 5, 0.10, 14, true, 1);
+    this.x = x;
+    this.y = y;
+    this.speed = 100;
+    this.game = game;
+    this.ctx = game.ctx;
+    // Entity.call(this, game, 0, 250);
+}
+//
+// Cell.prototype = new Entity();
+// Cell.prototype.constructor = Cell;
+
+Cell.prototype.draw = function () {
+    this.ctx.beginPath();
+    this.ctx.fillStyle = this.color;
+    this.ctx.arc(this.x + RADIUS, this.y + RADIUS, RADIUS, 0, Math.PI * 2, false);
+    this.ctx.fill();
+    this.ctx.closePath();
+}
+
+Cell.prototype.update = function () {
+    if (this.animation.elapsedTime < this.animation.totalTime * 8 / 14)
+        this.x += this.game.clockTick * this.speed;
+    if (this.x > 800) this.x = -230;
+}
+
+
+AM.queueDownload("./img/RobotUnicorn.png");
+AM.queueDownload("./img/guy.jpg");
+AM.queueDownload("./img/mushroomdude.png");
+AM.queueDownload("./img/runningcat.png");
+AM.queueDownload("./img/background.jpg");
+
+AM.downloadAll(function () {
+    var canvas = document.getElementById("gameWorld");
+    var ctx = canvas.getContext("2d");
+
+    var gameEngine = new GameEngine();
+    gameEngine.init(ctx);
+    gameEngine.start();
+
+    // gameEngine.addEntity(new Background(gameEngine, AM.getAsset("./img/background.jpg")));
+    // gameEngine.addEntity(new MushroomDude(gameEngine, AM.getAsset("./img/mushroomdude.png")));
+    // gameEngine.addEntity(new Cheetah(gameEngine, AM.getAsset("./img/runningcat.png")));
+    // gameEngine.addEntity(new Guy(gameEngine, AM.getAsset("./img/guy.jpg")));
+
+    console.log("All Done!");
+});
